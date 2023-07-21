6abf36e071fa628e661aab489da41bedbe03534e
jcdominguez <jcd.pro@gmail.com>
2023-07-21 14:38:16 +0200

    1er controller GET hello

---
+++ src/main/java/com/example/demo/controller/AnnuaireController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnnuaireController {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Coucou";
+    }
+}
