01ca93d2bb48e536fcc1e354657045a9d5a56e6f
jcdominguez <jcd.pro@gmail.com>
2023-07-21 14:46:46 +0200

    GET 1 personne

--- src/main/java/com/example/demo/controller/AnnuaireController.java
+++ src/main/java/com/example/demo/controller/AnnuaireController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.business.Personne;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,5 +13,12 @@ public class AnnuaireController {
     @GetMapping("hello")
     public String sayHello(){
         return "Coucou";
+    }
+
+    @GetMapping("personnes")
+    public Personne getPersonnes(){
+
+        Personne p = new Personne("Jean", "Rodriguez");
+        return p;
     }
 }
