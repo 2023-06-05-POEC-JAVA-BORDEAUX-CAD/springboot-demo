f93a3cab754a2c4937ebeac47661192376cd48df
jcdominguez <jcd.pro@gmail.com>
2023-07-21 15:07:08 +0200

    POST Personne

--- src/main/java/com/example/demo/controller/AnnuaireController.java
+++ src/main/java/com/example/demo/controller/AnnuaireController.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.business.Personne;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +27,11 @@ public class AnnuaireController {
         personnes.add(p2);
 
         return personnes;
+    }
+
+
+    @PostMapping("personnes")
+    public void postPersonne(@RequestBody Personne newPersonne){
+        System.out.println(newPersonne);
     }
 }
