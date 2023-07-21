6fe792e201ac1d38af588cb8a4f650b498e4412c
jcdominguez <jcd.pro@gmail.com>
2023-07-21 16:22:24 +0200

    ajout Controller Admin qui utilise le meme Service qui est un singleton

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.business.Annuaire;
+import com.example.demo.business.Personne;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+
+    @Autowired
+    Annuaire annuaire;
+
+    @GetMapping("personnes")
+    public List<Personne> getPersonnes(){
+        return annuaire.getPersonnes();
+    }
+}
--- src/main/java/com/example/demo/controller/AnnuaireController.java
+++ src/main/java/com/example/demo/controller/AnnuaireController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("api")
 public class AnnuaireController {
 
     @Autowired
